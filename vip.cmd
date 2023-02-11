5c72e1813cb03fe559bd691502476d8a566e4c96
420GhosTx <114801483+420GhosTx@users.noreply.github.com>
2023-02-11 06:33:06 +0100

    vipcmd

---
+++ vip.cmd
@@ -0,0 +1,7 @@
+echo "# 420GhosTx" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/420GhosTx/420GhosTx.git
+git push -u origin main
